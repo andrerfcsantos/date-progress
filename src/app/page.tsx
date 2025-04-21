7635d93ed93acf15a8ffd43e67501153d6c39381
André Santos <andrerfcsantos@gmail.com>
2025-04-21 12:45:50 +0100

    Configure prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,26 @@
-import * as React from "react";
+import * as React from 'react'
 
-import {BookCardProps, default as BookCard} from "@/components/design/bookcard";
+import {
+    BookCardProps,
+    default as BookCard,
+} from '@/components/design/bookcard'
 
 export default function Home() {
-
     const books: BookCardProps[] = [
         {
-            title: "Fundamentals of Software Architecture",
+            title: 'Fundamentals of Software Architecture',
             dateStart: new Date(Date.UTC(2025, 3, 14, 12, 0, 0)),
-            dateEnd: new Date(Date.UTC(2025, 6, 1, 0, 0, 0))
+            dateEnd: new Date(Date.UTC(2025, 6, 1, 0, 0, 0)),
         },
     ]
 
     return (
-        <div
-            className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-[16px] items-center sm:items-start">
-
                 {books.map((book, index) => (
-                    <BookCard key={index} {...book}/>
+                    <BookCard key={index} {...book} />
                 ))}
-
             </main>
         </div>
-    );
+    )
 }
